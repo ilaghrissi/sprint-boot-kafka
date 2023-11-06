e62b511359f625ebd1a585fd7aa8bac8dbe73e26
LAGHRISSI Imad <laghrissi.imad@gmail.com>
2023-11-06 14:37:21 +0100

    first commit

---
+++ src/main/java/com/example/sprintbootkafka/SprintBootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootKafkaApplication.class, args);
+	}
+
+}
